0f1578a3c2f7a081ce754c49eae2567ad43484c2
roscoericch <sikirurazak1@gmail.com>
2024-01-31 00:06:36 +0100

    update prime checker validation

--- PrimeChecker.java
+++ PrimeChecker.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PrimeChecker {
@@ -12,10 +13,20 @@ public class PrimeChecker {
         };
         return result;
     }
+    
     public static void main(String[] args) {
-         Scanner s = new Scanner(System.in);  
-       System.out.print("Enter a number : ");  
-       int n = s.nextInt();  
+       Scanner s = new Scanner(System.in);
+       int n;
+       while (true) {
+        System.out.print("Enter a number : ");
+        try {
+            n = s.nextInt();
+            break;
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter valid Number : ");
+            s.next();
+        }
+    } 
         boolean check = PrimeChecker.isPrime(n);
         System.out.println(check);
     }
